c426d9cffa7c8a6fd0409afeac7e175f4d4bae32
Fernando de Abreu Pereira <fernandodeabreupereira@gmail.com>
2015-11-20 08:55:15 -0200

    Adicionando classe de banco de dados

---
+++ clsBD.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace prjDoctorHelpDev
+{
+    class clsBD
+    {
+        static public SqlConnection AbreBanco()
+        {
+            SqlConnection conexao = new SqlConnection();
+            conexao.ConnectionString = "Data Source=(LocalDB)\\v11.0;AttachDbFilename=C:\\Users\\FernandoAbreuPereira\\Desktop\\prjDoctorHelpDev\\prjDoctorHelpDev\\BancoDH.mdf;Integrated Security=True;Connect Timeout=30";
+            conexao.Open();
+
+            return conexao;
+        }
+
+        static public void ExecutarSQL(string SQL)
+        {
+            SqlCommand comando = new SqlCommand();
+            comando.Connection = AbreBanco();
+            comando.CommandType = CommandType.Text;
+            comando.CommandText = SQL;
+            comando.ExecuteNonQuery();
+        }
+
+        static public SqlDataReader ExecutaConsultaDR(string SQL)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = AbreBanco();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = SQL;
+
+            SqlDataReader dr;
+            dr = cmd.ExecuteReader();
+
+            return dr;
+        }
+
+        static public DataTable ExecutaConsultaDT(string SQL)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = AbreBanco();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = SQL;
+
+            SqlDataAdapter da = new SqlDataAdapter();
+            da.SelectCommand = cmd;
+
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            return dt;
+        }
+    }
+}
